464cbb724880902d8f0d50c2c409541d99c4ccbf
Elton Jeff Bulotano <elton.jeff.bulotano@copypaste.ph>
2018-08-22 21:59:17 +0800

    Add service provider for package

---
+++ src/KladminServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Xeight8\Kladmin;
+
+use Illuminate\Support\ServiceProvider;
+
+class KladminServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
